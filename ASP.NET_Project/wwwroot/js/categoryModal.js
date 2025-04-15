9d8c4127bf46fd58a93122aacd66a0fae9c630f7
vladhardetskyy@gmail.com <13238274Vv>
2025-04-15 17:33:21 +0300

    Moved the modal window to a separate folder

---
+++ ASP.NET_Project/wwwroot/js/categoryModal.js
@@ -0,0 +1,10 @@
+﻿function openModal() {
+    const modal = new bootstrap.Modal(document.getElementById('categoryModal'));
+    modal.show();
+}
+
+function selectCategory(name) {
+    document.getElementById('categoryNameInput').value = name;
+    const modal = bootstrap.Modal.getInstance(document.getElementById('categoryModal'));
+    modal.hide();
+}
